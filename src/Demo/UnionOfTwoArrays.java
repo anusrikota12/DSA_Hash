4b45383d053e403af0d2f7b9fec48c0bf708f1c1
anusrikota12 <anusrikota68@gmail.com>
2026-09-15 13:42:57 +0530

    UnionOfTwoArrays

---
+++ src/Demo/UnionOfTwoArrays.java
@@ -0,0 +1,19 @@
+package Demo;
+import java.util.*;
+public class UnionOfTwoArrays {
+	public static Set<Integer> union(int[] a,int[] b) {
+		HashSet<Integer> set=new HashSet<>();
+		for(int num:a) {
+			set.add(num);
+		}
+		for(int num:b) {
+			set.add(num);
+		}
+		return set;
+	}
+	public static void main(String[] args) {
+		int[] a= {1,2,3,4};
+		int[] b= {4,5,6,7};
+		System.out.println(union(a,b));
+	}
+}
